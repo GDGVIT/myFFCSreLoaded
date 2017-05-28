e50292133d58f05956639b82ad5ddea83719aa3b
Reejh Ghosh <r4reejh@hotmail.com>
2017-05-28 14:26:09 +0530

    download option working on local

--- routes/index.js
+++ routes/index.js
@@ -252,7 +252,7 @@ router.post('/downloadtt',(req,res)=>{
 	phantomPrint.doPrint(uid)
 	.then(()=>{
 		console.log("done print")
-		res.send(req.session.passport.user)
+		res.send({"status":true, "link":"/download/"+req.session.passport.user})
 	})
 	.catch((e)=>{
 		console.log(e)
